6880592f8fe696b8e1b6b194073a07b8f40d6f6a
Kirti-321 <74565216+Kirti-321@users.noreply.github.com>
2020-11-17 02:26:56 +0530

    Conditional statement

---
+++ Conditional Statement.java
@@ -0,0 +1,12 @@
+class Stmt{
+public void conditionalStmt(){
+if(1<4)
+{
+System.out.println("Inside if");
+}
+else
+{
+System.out.println("Inside else");
+}
+System.out.println("This is from contributor");
+}
